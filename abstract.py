e3a171cca6bbe0d07309e6491732192fd17d80ee
Md. Yasmi Tohabar Evon (2016831021) <silverevon10010@gmail.com>
2021-12-02 01:14:24 +0600

    abstraction

---
+++ abstract.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Square(Shape):
+    def __init__(self, side):
+        self.__side = side
+
+    def area(self):
+        return self.__side ** 2
+
+    def perimeter(self):
+        return 4 * self.__side
+
+
+# my_shape = Shape()
+my_square = Square(5)
+print(my_square.area())
+print(my_square.perimeter())
